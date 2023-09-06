a7d3b297aa7e5462c745fdb837ae9e9cfdbdc215
cristi40 <ccristescu40@gmail.com>
2023-09-06 11:02:37 +0300

    cev

--- proiect/instruction_memory.v
+++ proiect/instruction_memory.v
@@ -1,11 +1,21 @@
-module instruction_memory(input wire [31:0] readAdress,
+module instruction_memory#(parameter SIZE = 128)(input wire [31:0] readAdress,
                             input wire clk,
                             input wire reset,
                             output reg [31:0] instruction);
-reg [31:0] [127:0] instructions;
-always @(readAdress)begin 
-    if(readAdress/(32'h4) <= 127)begin 
-        instruction <= instructions[readAdress/4];
+reg [31:0] [SIZE-1:0] instructions;
+reg [31:0] index;
+always @(readAdress, posedge clk)begin 
+    if(reset)begin 
+        for( index = 0 ; index < SIZE ; index = index+1) begin
+                instructions[index] = 0;
+        end
+        index <= 32'b0;
+    end else begin 
+        if(readAdress/(32'h4) <= SIZE-1)begin 
+            instruction <= instructions[readAdress/4];
+        end else begin 
+            instruction <= instructions[32'b0];
+        end
     end
 end
 endmodule
--- proiect/register_file.v
+++ proiect/register_file.v
@@ -1,24 +1,24 @@
 module register_file(input wire clk,
-                    input wire [4:0] regs,
-                    input wire [4:0] regt,
-                    input wire [4:0] regd,
-                    input wire regWrite,
-                    input wire [31:0] dataWrite,
-                    output reg [31:0] reg1,
-                    output reg [31:0] reg2
-                    );
+                    input wire [31:0] regs,//instruction
+                    input wire [31:0] regt,//instruction
+                    input wire [31:0] regd,//instruction
+                    input wire regWrite,//semnal ce indica scrierea in registrul destinatie
+                    input wire [31:0] dataWrite,//rezultat
+                    output reg [31:0] reg1,//continut regs
+                    output reg [31:0] reg2//continut regd
+                    );//front pozitiv, scriere la front positiv regWrite
 reg [31:0] [31:0] registerMemory;
 
-always @(posedge clk, regs, regt, regd, posedge regWrite) begin 
-    if(regs <= 5'b11111 && regt <= 5'b11111)begin 
-        reg1 <= registerMemory[regs];
-        reg2 <= registerMemory[regt];
+always @(posedge clk) begin 
+    if(regs[25:21] <= 5'b11111 && regt[20:16] <= 5'b11111)begin 
+        reg1 <= registerMemory[regs[25:21]];
+        reg2 <= registerMemory[regt[20:16]];
+    end else begin 
+        reg1 <= 32'b0;
+        reg2 <= 32'b0;
+    end
+    if(regd[15:11] <= 5'b11111 && regWrite == 1'b1) begin
+        registerMemory[regd[15:11]]<=dataWrite;
     end
 end
-always @(posedge clk , posedge regWrite, dataWrite, regd)begin 
-    if(regd <= 5'b11111) begin
-        registerMemory[regd]<=dataWrite;
-    end
-end
-
 endmodule
