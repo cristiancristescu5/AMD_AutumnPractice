35577a719580b54ed2da231565d8cce029e1ea10
cristi40 <ccristescu40@gmail.com>
2023-09-07 11:54:56 +0300

    ceva

--- proiect/program_counter.v
+++ proiect/program_counter.v
@@ -3,7 +3,11 @@ module program_counter(input wire clk,
                         input wire [31:0] nextInstruction,
                         output reg [31:0] currentInstruction);
 reg [31:0] instruction;
-always @(posedge clk)begin
+initial begin 
+    instruction <= 32'b0;
+    currentInstruction <= 32'b0;
+end
+always @(posedge clk, nextInstruction)begin
     if(reset)begin 
         instruction <= 32'b0;
         currentInstruction <= 32'b0;
--- proiect/register_file.v
+++ proiect/register_file.v
@@ -1,7 +1,7 @@
 module register_file(input wire clk,
-                    input wire [31:0] regs,//instruction
-                    input wire [31:0] regt,//instruction
-                    input wire [31:0] regd,//instruction
+                    input wire [5:0] regs,//instruction
+                    input wire [5:0] regt,//instruction
+                    input wire [5:0] regd,//instruction
                     input wire regWrite,//semnal ce indica scrierea in registrul destinatie
                     input wire [31:0] dataWrite,//rezultat
                     output reg [31:0] reg1,//continut regs
@@ -10,15 +10,15 @@ module register_file(input wire clk,
 reg [31:0] [31:0] registerMemory;
 
 always @(posedge clk) begin 
-    if(regs[25:21] <= 5'b11111 && regt[20:16] <= 5'b11111)begin 
-        reg1 <= registerMemory[regs[25:21]];
-        reg2 <= registerMemory[regt[20:16]];
+    if(regs <= 5'b11111 && regt <= 5'b11111)begin 
+        reg1 <= registerMemory[regs];
+        reg2 <= registerMemory[regt];
     end else begin 
         reg1 <= 32'b0;
         reg2 <= 32'b0;
     end
-    if(regd[15:11] <= 5'b11111 && regWrite == 1'b1) begin
-        registerMemory[regd[15:11]]<=dataWrite;
+    if(regd <= 5'b11111 && regWrite == 1'b1) begin
+        registerMemory[regd]<=dataWrite;
     end
 end
 endmodule
